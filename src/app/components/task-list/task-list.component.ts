00cde303d0f47254883691b7119d69ad8810c8af
Irene <UsuarioPC@MAKIWAZOWSKI>
2022-12-07 13:16:24 +0100

    add tasks to task list and pass them to child comp

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ITask, Levels } from 'src/app/models/interfaces/Task.interface';
 
 @Component({
   selector: 'app-task-list',
@@ -6,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent implements OnInit{
-  
+  /* TODO: reformular como una lista de tareas */
+  //creamos tareas que habra que pasar al componente hijo task
+  task1: ITask = {
+    title: 'Task 1',
+    description: 'Description 1',
+    completed: false,
+    level: Levels.Info //importamos levels
+  }
+  task2: ITask = {
+    title: 'Task 2',
+    description: 'Description 2',
+    completed: false,
+    level: Levels.Urgent //importamos levels
+  }
+
   constructor(){}
   
   ngOnInit(): void {
