9a7c0dc4738c2e927a4b9222b267fd151a5061da
Irene <UsuarioPC@MAKIWAZOWSKI>
2022-12-07 13:10:03 +0100

    edit code tasks, add input, create delete method

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -8,10 +8,20 @@ import { ITask } from 'src/app/models/interfaces/Task.interface';
 })
 export class TaskComponent implements OnInit{
 
+  /* usamos undefined para no ponerle un valor estricto , en este caso ponemos que es o indefinido o un ITask
+  Ej. definir: { title : '' , description: ''}
+  */
+  @Input() task: ITask | undefined ; 
+
   constructor(){}
 
   ngOnInit(): void {
     
+  }
+
+  /* metodo para eliminar tarea */
+  deleteTask(){
+    console.log('Delete a task', this.task?.title)
   }
 
 }
